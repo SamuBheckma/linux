596709aaaa56e50e8262c9fb4237018c1efe2398
Samuel Bheckma <samuelb.conta@gmail.com>
2025-12-04 10:02:12 -0300

    linux flash install 0.1
    Install zsh, curl, git, and additional tools. Set up Oh My Zsh with Spaceship theme and plugins.

---
+++ linux.sh
@@ -0,0 +1,12 @@
+sudo apt-get install zsh
+zsh --version
+sudo apt-get install curl
+curl --version
+sudo add-apt-repository ppa:git-core/ppa ; sudo apt update ; sudo apt-get install git
+git --version
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+sudo apt-get install micro
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
