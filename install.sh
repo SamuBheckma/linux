5c27fec13b47ece0e8ffff0a9bb950c25f039775
Sawyl <samuelb.conta@gmail.com>
2026-05-22 19:16:47 -0300

    Refactor install script for package installation
    Removed unnecessary packages from installation and simplified theme symlink creation.

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
-
 info() { echo -e "\033[1;34m→\033[0m $*"; }
 success() { echo -e "\033[1;32m✓\033[0m $*"; }
 
@@ -10,7 +8,7 @@ info "Instalando pacotes essenciais..."
 sudo apt install -y \
   base-devel git neovim \
   zsh kitty \
-  curl wget fzf
+  curl wget
 
 success "Pacotes base instalados"
 
@@ -27,14 +25,9 @@ ZSH_CUSTOM=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
 [ ! -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ] && \
   git clone https://github.com/zsh-users/zsh-autosuggestions.git "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
 
-ST_DIR="$ZSH_CUSTOM/themes/spaceship-prompt"
-[ ! -d "$ST_DIR" ] && git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ST_DIR" --depth=1
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
 
-if [ ! -L "$ZSH_CUSTOM/themes/spaceship.zsh-theme" ]; then
-  ln -sf "$ST_DIR/spaceship.zsh.theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
-fi
-
-[ ! -d "$HOME/.fzf" ] && git clone --depth 1 https://github.com/junegunn/fzf.git "$HOME/.fzf"
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 
 [ "$SHELL" != "$(which zsh)" ] && chsh -s "$(which zsh)"
 
